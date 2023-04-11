74f37e4d29dbbfdec38468e49e2cca562d633ab3
Aliimranatabey <aliimranatabey@gmail.com>
2023-04-11 23:32:59 +0300

    start of encapsulation

--- OOPGiris/src/Araba.java
+++ OOPGiris/src/Araba.java
@@ -1,10 +1,51 @@
 
 public class Araba {
 
-	public String color;
+	/*public String color;
 	public int doors;
 	public int wheels;
 	public String engine;
-	public String model;
+	public String model;*/
+	
+	private String color;
+	private int doors;
+	private int wheels;
+	private String engine;
+	private String model;
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getDoors() {
+		return doors;
+	}
+	public void setDoors(int doors) {
+		this.doors = doors;
+	}
+	public int getWheels() {
+		return wheels;
+	}
+	public void setWheels(int wheels) {
+		if(wheels<=0) {
+			System.out.println("Tekerlekler 0 dan küçük olamaz");
+		}else {
+		this.wheels = wheels;
+		}
+	}
+	public String getEngine() {
+		return engine;
+	}
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
 	
 }
--- OOPGiris/src/Test.java
+++ OOPGiris/src/Test.java
@@ -3,12 +3,31 @@ public class Test {
 	public static void main(String[] args) {
 		
 		Araba araba1=new Araba();
-		araba1.color="Red";
+		/*araba1.color="Red";
 		araba1.doors=4;
 		araba1.wheels=4;
 		araba1.engine="16V";
-		araba1.model="Renaultt";
+		araba1.model="Renaultt";*/
 		
-		System.out.println(araba1.engine);
+		araba1.setEngine("16V");
+		araba1.setWheels(-4);
+		System.out.println(araba1.getEngine());
+		
+		/*
+		araba2 diye bir referans oluşturmuşsun ama herhangi bir objeyi buna atamamışsın .Yani araba2 herhangi bir objeyi göstermiyor .
+		
+		Araba araba2;
+		araba2.setEngine("16V");*/
+	
+		/*
+		NullPointerException : senin bir referansın var ancak o referansa herhangi bir obje ataması yapmadan sen bu referansa erişmeye çalışıyorsun . 
+		Yani bir tane obje varmış gibi o referansın methotlarına erişmeye çalışıyorsun . (ANDROID TARAFINDA ÇOKÇA ALINAN BİR HATA )
+		
+		Araba araba3=null;
+		araba3.setDoors(2);
+		 */
+		
+		
+		
 	}
 }
