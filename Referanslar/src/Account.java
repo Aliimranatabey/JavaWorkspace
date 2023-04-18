ca59a7609b5e7da9b1b431a030b6cb0c74f9fdf9
Aliimranatabey <aliimranatabey>
2023-04-18 16:30:07 +0300

    object referances

---
+++ Referanslar/src/Account.java
@@ -0,0 +1,69 @@
+
+public class Account {
+	private String accountNumber;
+	private double balance;
+	private String name;
+	private String email;
+	private String phoneNumber;
+	
+	public void depositMoney(double amount) {
+		balance+=amount;
+		System.out.println("New Balance = "+balance);
+	}
+	
+	public void withdrawMoney(double amount) {
+		if(amount>1500) {
+			System.out.println("Bir günde 1500 TL'den fazla para çekemezsiniz...");
+		}
+		if(balance-amount<0) {
+			System.out.println("Yeterli bakiye yok . Bakiye = "+balance);
+		}else {
+		balance-=amount;
+		System.out.println("Yeni Bakiye = "+balance);
+		}
+	}
+	
+	public Account() {
+		System.out.println("Kendi Yazdığımız Constructor....");
+	}
+	
+	public Account(String accountNumber,double balance,String name,String email,String phoneNumber) {
+		System.out.println("Parametreli Constructor....");
+		this.accountNumber=accountNumber;
+		this.balance=balance;
+		this.name=name;
+		this.email=email;
+		this.phoneNumber=phoneNumber;
+	}
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
---
+++ Referanslar/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Account account1=new Account("123123123123",123,"Mustafa Murat Coşkun","coskun.m.murat@gmail.com","05555555555");
+		Account account2=account1;
+		Account account3=new Account();
+		//account2 ve account1 referansları aynı objeyi gösteriyorlar
+		//account3 ve account1 referansları aynı objeyi göstermiyorlar
+		//if içerisinde sorulan soru account1 ve account2 birbirine eşit midir değil , bu iki referans aynı objeyi gösteriyor mu diye soruyoruz .
+		if(account1==account2) {
+			System.out.println("Referanslar Aynı Objeyi Gösteriyorlar ...");
+		}else {
+			System.out.println("Aynı objeyi göstermiyorlar ...");
+		}
+		new Account("1455555553123",123,"Ahmet Can Demir","ahmetcandemir@gmail.com","05555333355").depositMoney(45);
+
+	}
+}
