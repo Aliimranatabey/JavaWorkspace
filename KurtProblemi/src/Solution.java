142b724f26e2e23246372721d481a28dc7e08613
Aliimranatabey <aliimranatabey@gmail.com>
2023-06-07 22:51:44 +0300

    kurtProblemi added

--- KurtProblemi/src/Solution.java
+++ KurtProblemi/src/Solution.java
@@ -14,44 +14,37 @@ class Result {
 	public static int uniqueWolfs(List<Integer> arr) {
 		Map<Integer, Integer> counter = new HashMap<>();
 
-        for (int tur : arr) {
-            counter.put(tur, counter.getOrDefault(tur, 0) + 1);
-        }
+		for (int tur : arr) {
+			counter.put(tur, counter.getOrDefault(tur, 0) + 1);
+		}
 
-        int enSikTur = Collections.max(counter.entrySet(), Map.Entry.comparingByValue()).getKey();
-        int enDusukID = Integer.MAX_VALUE;
+		int enSıkTur = Collections.max(counter.entrySet(), Map.Entry.comparingByValue()).getKey();
+		int enDusukID = Integer.MAX_VALUE;
 
-        for (int tur : arr) {
-            if (tur == enSikTur && tur < enDusukID) {
-                enDusukID = tur;
-            }
-        }
+		for (int tur : arr) {
+			if (tur == enSıkTur && tur < enDusukID) {
+				enDusukID = tur;
+			}
+		}
 
-        return enDusukID;
+		return enDusukID;
 	}
 }
 
 public class Solution {
 
 	public static void main(String[] args) throws IOException {
-		
-		try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("D:\\KurtProblemi.txt"));
 
-            int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
-            List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt).collect(toList());
-
-            int result = Result.uniqueWolfs(arr);
-
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
-
-            bufferedReader.close();
-            bufferedWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("D:\\KurtProblemi.txt"));
+		int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+		List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+				.map(Integer::parseInt).collect(toList());
+		int result = Result.uniqueWolfs(arr);
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+		bufferedReader.close();
+		bufferedWriter.close();
 
 	}
 }
